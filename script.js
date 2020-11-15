ff50265f23334238ad0fb5823aac44a51ead4adc
sarthakkundra <sarthakkundra21@gmail.com>
2020-11-15 18:38:30 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Variables :-
+1) Screen resolution
+2) Tracker
+3) Video screen
+4) Filters
+5) Selectors
+*/
+
+// Load images for filters
+
+// Setup video feed, filter selector etc
+
+// Draw the filter
+
+// Place the filter
